b39b93d81c67c50532f91110450e6744643f72b5
CestPasFo <c.jaworski12@hotmail.fr>
2024-12-03 12:04:36 +0100

    Fix: Misc

--- migrations/Version20241203101050.php
+++ migrations/Version20241203101050.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241202092701 extends AbstractMigration
+final class Version20241203101050 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,11 +23,11 @@ final class Version20241202092701 extends AbstractMigration
         $this->addSql('CREATE TABLE chatroom (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, _user_id INT NOT NULL, _chatroom_id INT NOT NULL, content LONGTEXT NOT NULL, sent_at DATETIME NOT NULL, INDEX IDX_B6BD307FD32632E8 (_user_id), INDEX IDX_B6BD307F944512CF (_chatroom_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user_chatroom (id INT AUTO_INCREMENT NOT NULL, _user_id INT NOT NULL, _chatroom_id INT NOT NULL, last_read DATETIME DEFAULT NULL, INDEX IDX_FFB38AC4D32632E8 (_user_id), INDEX IDX_FFB38AC4944512CF (_chatroom_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_chatroom (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, chatroom_id INT NOT NULL, last_read DATETIME DEFAULT NULL, INDEX IDX_FFB38AC4A76ED395 (user_id), INDEX IDX_FFB38AC4CAF8A031 (chatroom_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FD32632E8 FOREIGN KEY (_user_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F944512CF FOREIGN KEY (_chatroom_id) REFERENCES chatroom (id)');
-        $this->addSql('ALTER TABLE user_chatroom ADD CONSTRAINT FK_FFB38AC4D32632E8 FOREIGN KEY (_user_id) REFERENCES `user` (id)');
-        $this->addSql('ALTER TABLE user_chatroom ADD CONSTRAINT FK_FFB38AC4944512CF FOREIGN KEY (_chatroom_id) REFERENCES chatroom (id)');
+        $this->addSql('ALTER TABLE user_chatroom ADD CONSTRAINT FK_FFB38AC4A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE user_chatroom ADD CONSTRAINT FK_FFB38AC4CAF8A031 FOREIGN KEY (chatroom_id) REFERENCES chatroom (id)');
     }
 
     public function down(Schema $schema): void
@@ -35,8 +35,8 @@ final class Version20241202092701 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FD32632E8');
         $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F944512CF');
-        $this->addSql('ALTER TABLE user_chatroom DROP FOREIGN KEY FK_FFB38AC4D32632E8');
-        $this->addSql('ALTER TABLE user_chatroom DROP FOREIGN KEY FK_FFB38AC4944512CF');
+        $this->addSql('ALTER TABLE user_chatroom DROP FOREIGN KEY FK_FFB38AC4A76ED395');
+        $this->addSql('ALTER TABLE user_chatroom DROP FOREIGN KEY FK_FFB38AC4CAF8A031');
         $this->addSql('DROP TABLE chatroom');
         $this->addSql('DROP TABLE message');
         $this->addSql('DROP TABLE `user`');

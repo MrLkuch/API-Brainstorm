4192334f4ee270ca3bcd2e4b10fcd0723e8a02e5
MrLkuch <kucheida.lucas@gmail.com>
2024-12-05 10:01:59 +0100

    Creation of About page

---
+++ src/Controller/AProposController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AProposController extends AbstractController
+{
+    #[Route('/a-propos', name: 'app_a_propos')]
+    public function index(): Response
+    {
+        return $this->render('a_propos/index.html.twig');
+    }
+}
